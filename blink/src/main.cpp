513d65abc3ff94ee54367c5991e82cc511595bf2
al3x76 <alexoiupompiliu@gmail.com>
2021-01-03 15:42:34 +0200

    blink

--- blink/src/main.cpp
+++ blink/src/main.cpp
@@ -5,8 +5,15 @@ void setup() {
   // put your setup code here, to run once:
   // start virtual serial communication
   Serial.begin(9600);
+  // set outpin pin
+  pinMode(ledPinBlink,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(ledPinBlink, HIGH);
+  delay(500);
+  digitalWrite(ledPinBlink, LOW);
+  delay(500);
+
 }
